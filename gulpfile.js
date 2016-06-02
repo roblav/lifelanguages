8e6ed15e6eebc16eeceb063d4e96141091f5b3b6
Rob Lavender <robert.lavender@digital.hmrc.gov.uk>
2016-06-02 16:03:10 +0100

    Add nunjucks.

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp =  require('gulp'),
+    nunjucksRender = require('gulp-nunjucks-render');
+
+gulp.task('nunjucks', function() {
+    // Gets .html and .nunjucks files in pages
+    return gulp.src('app/pages/**/*.+(html|nunjucks)')
+        // Renders template with nunjucks
+        .pipe(nunjucksRender({
+            path: ['app/templates']
+        }))
+        // output files in app folder
+        .pipe(gulp.dest('app'))
+});
+
+gulp.task('default', ['start']);
